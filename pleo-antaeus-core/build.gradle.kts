c014a0e9ac7e6a35396b2431ca3cf2ffdac1495a
Felix Brousseau <felix.brousseau-tremblay.1@live.com>
2019-04-20 00:11:23 +0200

    Added Arrow - updated Readme

--- pleo-antaeus-core/build.gradle.kts
+++ pleo-antaeus-core/build.gradle.kts
@@ -1,10 +1,20 @@
 plugins {
     kotlin("jvm")
+    kotlin("kapt")
 }
 
 kotlinProject()
 
+val arrow_version = "0.9.0"
 dependencies {
     implementation(project(":pleo-antaeus-data"))
     compile(project(":pleo-antaeus-models"))
+
+    compile("io.arrow-kt:arrow-core-data:$arrow_version")
+    compile ("io.arrow-kt:arrow-core-extensions:$arrow_version")
+    compile ("io.arrow-kt:arrow-syntax:$arrow_version")
+    compile ("io.arrow-kt:arrow-typeclasses:$arrow_version")
+    compile ("io.arrow-kt:arrow-extras-data:$arrow_version")
+    compile ("io.arrow-kt:arrow-extras-extensions:$arrow_version")
+    kapt    ("io.arrow-kt:arrow-meta:$arrow_version")
 }

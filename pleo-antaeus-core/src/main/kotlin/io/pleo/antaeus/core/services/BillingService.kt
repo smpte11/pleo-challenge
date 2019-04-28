74f639799afe080687a77373ee46a8414fda4f60
Felix Brousseau <felix.brousseau-tremblay.1@live.com>
2019-04-28 21:54:36 +0200

    Implemented bill skeleton - Solved Arrow downcasting problem (Kind<List, Invoice> -> List<Invoice> - Lifted sequence to concurrency primitive - Added unit test

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -2,8 +2,17 @@ package io.pleo.antaeus.core.services
 
 import arrow.effects.IO
 import arrow.typeclasses.*
+import arrow.data.extensions.list.functor.map
+import arrow.effects.extensions.io.fx.fx
 import io.pleo.antaeus.core.external.PaymentProvider
 
-class BillingService<F>(AP: Applicative<F>, paymentProvider: PaymentProvider): Applicative<F> by AP {
-    var pp: IO<PaymentProvider> = IO.just(paymentProvider)
+class BillingService<F>(M: Monad<F>, private val paymentProvider: PaymentProvider) : Monad<F> by M {
+    fun bill(invoiceService: InvoiceService, customerService: CustomerService) = fx {
+        val customers = customerService.fetchAll()
+        val invoices = customers.map { invoiceService.fetch(it.id) }
+        val paymentResults = !NonBlocking.parSequence(
+                invoices.map { IO.just(paymentProvider.charge(it)) }
+        )
+        paymentResults
+    }
 }
--- pleo-antaeus-core/src/test/kotlin/io/pleo/antaeus/core/services/BillingServiceTest.kt
+++ pleo-antaeus-core/src/test/kotlin/io/pleo/antaeus/core/services/BillingServiceTest.kt
@@ -1,20 +1,50 @@
 package io.pleo.antaeus.core.services
 
 import arrow.data.ListK
-import arrow.data.extensions.listk.applicative.applicative
-import arrow.effects.IO
+import arrow.data.extensions.listk.monad.monad
+import arrow.data.k
+import io.mockk.every
 import io.mockk.mockk
 import io.pleo.antaeus.core.external.PaymentProvider
+import io.pleo.antaeus.data.AntaeusDal
+import io.pleo.antaeus.models.*
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
+import kotlin.random.Random
+
+fun randomCurrency(): Currency {
+    return Currency.values()[Random.nextInt(0, Currency.values().size)]
+}
 
 internal class BillingServiceTest {
     private val provider = mockk<PaymentProvider>()
+    private val invoiceService = mockk<InvoiceService> {
+        every { fetch(any()) } returns Invoice(
+                id = Random.nextInt(),
+                customerId = Random.nextInt(),
+                amount = Money(100.toBigDecimal(), randomCurrency()),
+                status = InvoiceStatus.PENDING
+        )
+    }
+    private  val customerService = mockk<CustomerService> {
+        every { fetchAll() } returns List(10) {
+            Customer(it, randomCurrency() )
+        }
+    }
 
     @Test
     fun `should init properly`() {
-        val billingService = BillingService(ListK.applicative(), provider)
+        /**
+         */
+        val billingService = BillingService(ListK.monad(), provider)
         assertNotNull(billingService)
+    }
+
+    @Test
+    fun `should bill customers`() {
+        val billingService = BillingService(ListK.monad(), provider)
+        val result = billingService.bill(invoiceService, customerService)
+        print(result)
     }
 }
